852ec7bf17b09d6ce7a7849f331dc9e3eeea12ff
Alexei Vinnikov <eternal.creature.vin@gmail.com>
2024-01-19 22:18:26 +0300

    Лабораторная работа 1

---
+++ Lab01_1/nForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab01_1
+{
+    public partial class nForm : Form
+    {
+        private bool nClose = false;
+        
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //не работает - на закрытии окна сюда не заходит
+        //в nForm.Designer.cs закоментировал внутренности в
+        //protected override void Dispose(bool disposing)
+        //и засунул в него метод Hide();
+        private void nForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Hide();
+            e.Cancel = true;
+        }
+
+        //метод из stackoverflow для выявления, какой метод вывляется призакрытии
+        /*protected override void DestroyHandle()
+        {
+            System.Diagnostics.Debugger.Break();
+            base.DestroyHandle();
+        }*/
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            _ = this.checkboxClose.Checked ? nClose = true : nClose = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nClose = true;
+            Close();
+        }
+    }
+}
--- Lab01_1/obj/Debug/net8.0-windows/Lab01_1.AssemblyInfo.cs
+++ Lab01_1/obj/Debug/net8.0-windows/Lab01_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab01_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6364450991bf72b0977b26e9ac970e7054511a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab01_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab01_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
